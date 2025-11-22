a324bd720b462f864eaa0d0b11b76605b600e9b1
[dyad] <pedroantonio.bsena@gmail.com>
2025-11-22 13:00:47 -0300

    [dyad] Atualizando link do botão do WhatsApp - wrote 1 file(s)

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -7,7 +7,7 @@ const WhatsAppIcon = () => (
 export const WhatsAppButton = () => {
     return (
         <a 
-            href="https://wa.me/5511999999999" // Replace with your WhatsApp number
+            href="https://api.whatsapp.com/send/?phone=5538998940253&text=Ol%C3%A1%2C+vim+do+instagram.+%0AGostaria%E2%80%A6&type=phone_number&app_absent=0"
             target="_blank" 
             rel="noopener noreferrer"
             className="fixed bottom-6 right-6 z-50"
